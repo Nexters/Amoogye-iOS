a9ffc81cda70a870248866939e0f087f3591cc7f
tngusmiso <k1miso012@gmail.com>
2019-07-13 15:43:20 +0900

    init project - 아무계 iOS 환경 세팅

---
+++ Amoogye/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Amoogye
+//
+//  Created by 임수현 on 13/07/2019.
+//  Copyright © 2019 KookKook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Amoogye-iOS
+
+> Nexters 15th KookKook Team. <br/> 
+> Amoogye iOS Project Repo
+
+## Member
+* 김준희
+* 임수현
+
+## Skill
+* [Swift](https://developer.apple.com/kr/swift/) : Main Languag
+
