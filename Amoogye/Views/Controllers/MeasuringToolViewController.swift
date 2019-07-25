5f0ddcf4b3f2e24d4b078ab69b830814bf3ba38f
tngusmiso <k1miso012@gmail.com>
2019-07-25 19:58:48 +0900

    iOS- [초기세팅] 첫 화면 탭바 세팅

---
+++ Amoogye/Views/Controllers/MeasuringToolViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MeasuringToolViewController.swift
+//  Amoogye
+//
+//  Created by 임수현 on 25/07/2019.
+//  Copyright © 2019 KookKook. All rights reserved.
+//
+
+import UIKit
+
+class MeasuringToolViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
