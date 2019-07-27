961dc57d5bec0f41fd14701115da7141b2021323
tngusmiso <k1miso012@gmail.com>
2019-07-27 23:26:13 +0900

    iOS-#4 [계산기] 계량하기 팁 ViewController 추가

---
+++ Amoogye/Views/Controllers/CalculatorMeasuringTipViewController.swift
@@ -0,0 +1,23 @@
+//
+//  CalculatorMeasuringTipViewController.swift
+//  Amoogye
+//
+//  Created by 임수현 on 27/07/2019.
+//  Copyright © 2019 KookKook. All rights reserved.
+//
+
+import UIKit
+
+class CalculatorMeasuringTipViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func closeButtonClick(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+
+}
