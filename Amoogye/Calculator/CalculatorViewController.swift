1087986da3b9f72afd7ae40ad6279a99c78ce863
hihiboss <junhui820@gmail.com>
2019-08-02 04:28:09 +0900

    iOS-#9 [Calculator] Implement numeric keyboard wireframe with collection view

--- Amoogye/Calculator/CalculatorViewController.swift
+++ Amoogye/Calculator/CalculatorViewController.swift
@@ -9,9 +9,87 @@
 import UIKit
 
 class CalculatorViewController: UIViewController {
+    var numericKeyboardButtonTitles = [String]()
+
+    @IBOutlet weak var numericKeyboardView: UICollectionView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        setupNumericKeyboard()
+    }
+
+    private func setupNumericKeyboard() {
+        numericKeyboardButtonTitles = makeNumericKeyboardButtonTitles()
+
+        let nibCell = UINib(nibName: "NumericKeyboardCollectionViewCell", bundle: nil)
+        self.numericKeyboardView.register(nibCell, forCellWithReuseIdentifier: "NumericKeyboardCollectionViewCell")
+
+        self.numericKeyboardView.delegate = self
+        self.numericKeyboardView.dataSource = self
+    }
+}
+
+extension CalculatorViewController: UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return numericKeyboardButtonTitles.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "NumericKeyboardCollectionViewCell", for: indexPath) as! NumericKeyboardCollectionViewCell
+        let buttonTitle = numericKeyboardButtonTitles[indexPath.item]
+
+        return cellWithNumericKeyboardButtonTitle(cell, with: buttonTitle)
+    }
+}
+
+extension CalculatorViewController: UICollectionViewDelegate {
+
+}
+
+extension CalculatorViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let cellWidth = collectionView.frame.width / 3 - 1
+        let cellHeight = collectionView.frame.height / 4 - 1
+
+        return CGSize(width: cellWidth, height: cellHeight)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 1.0
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 1.0
+    }
+}
+
+extension CalculatorViewController {
+    private func makeNumericKeyboardButtonTitles() -> [String] {
+        var contents: [String] = []
+
+        for i in 1..<10 {
+            contents.append(String(i))
+        }
+        contents.append("0")
+        contents.append(".")
+        contents.append("del")
+
+        return contents
+    }
+
+    private func cellWithNumericKeyboardButtonTitle(_ cell: NumericKeyboardCollectionViewCell, with buttonTitle: String) -> NumericKeyboardCollectionViewCell {
+        if buttonTitle == "del" {
+            cell.keyboardButton.setImage(UIImage(named: "keyboardBackspace"), for: .normal)
+            cell.keyboardButton.setTitle("", for: .normal)
+            return cell
+        }
+
+        cell.keyboardButton.setTitle(buttonTitle, for: .normal)
+        return cell
     }
 }
