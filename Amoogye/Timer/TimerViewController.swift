623db0ed2606c0730e00c349002150569f0f13e1
tngusmiso <k1miso012@gmail.com>
2019-08-07 17:06:59 +0900

    iOS-#15 [타이머] Numeric Keyboard

--- Amoogye/Timer/TimerViewController.swift
+++ Amoogye/Timer/TimerViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class TimerViewController: UIViewController {
 
+    var numericKeyboardButtonTitles = [String]()
+
     var timerModel = TimerModel()
     let colorGreen = UIColor(displayP3Red: 109/255, green: 212/255, blue: 0, alpha: 1)
 
@@ -27,8 +29,12 @@ class TimerViewController: UIViewController {
     @IBOutlet weak var timerView: UIView!
     @IBOutlet weak var timerLabel: UILabel!
 
+    @IBOutlet weak var numericKeyboardView: UICollectionView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        setupNumericKeyboard()
         setUpTextfield()
         setButtonStyle(startButton, pauseButton, cancelButton)
     }
@@ -55,10 +61,12 @@ class TimerViewController: UIViewController {
     @IBAction func clickStart(_ sender: Any) {
         if timerModel.isWorkingTimer {
             checkFinishTimer()
+            numericKeyboardView.isHidden = false
         } else if getSecond() > 0 {
             startTimer()
             showPauseButton()
             setTimerTextfieldsEnable(booltype: false)
+            numericKeyboardView.isHidden = true
         }
     }
 
@@ -77,6 +85,7 @@ class TimerViewController: UIViewController {
         cancelTimer()
         showStartButton()
         setTimerTextfieldsEnable(booltype: true)
+        numericKeyboardView.isHidden = false
     }
 }
 
@@ -202,5 +211,84 @@ extension TimerViewController {
         alertLabel.text = "알려드릴게요!"
         startButton.setTitle("시작하기", for: .normal)
         setTimerTextfieldsEnable(booltype: true)
+    }
+}
+
+// MARK: - numeric keyboard
+extension TimerViewController {
+
+    private func hideKeyboards() {
+        numericKeyboardView.isHidden = true
+    }
+
+    private func setupNumericKeyboard() {
+        numericKeyboardButtonTitles = makeNumericKeyboardButtonTitles()
+
+        let nibCell = UINib(nibName: "NumericKeyboardCollectionViewCell", bundle: nil)
+        self.numericKeyboardView.register(nibCell, forCellWithReuseIdentifier: "NumericKeyboardCollectionViewCell")
+
+        self.numericKeyboardView.delegate = self
+        self.numericKeyboardView.dataSource = self
+    }
+    private func makeNumericKeyboardButtonTitles() -> [String] {
+        var contents: [String] = []
+
+        for i in 1..<10 {
+            contents.append(String(i))
+        }
+        contents.append("0")
+        contents.append(".")
+        contents.append("del")
+
+        return contents
+    }
+
+    private func cellWithNumericKeyboardButtonTitle(_ cell: NumericKeyboardCollectionViewCell, with buttonTitle: String) -> NumericKeyboardCollectionViewCell {
+        if buttonTitle == "del" {
+            cell.keyboardButton.setImage(UIImage(named: "keyboardBackspace"), for: .normal)
+            cell.keyboardButton.setTitle("", for: .normal)
+            return cell
+        }
+
+        cell.keyboardButton.setTitle(buttonTitle, for: .normal)
+        return cell
+    }
+}
+
+extension TimerViewController: UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return numericKeyboardButtonTitles.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "NumericKeyboardCollectionViewCell", for: indexPath) as! NumericKeyboardCollectionViewCell
+        let buttonTitle = numericKeyboardButtonTitles[indexPath.item]
+
+        return cellWithNumericKeyboardButtonTitle(cell, with: buttonTitle)
+    }
+}
+
+extension TimerViewController: UICollectionViewDelegate {
+
+}
+
+extension TimerViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let cellWidth = collectionView.frame.width / 3 - 1
+        let cellHeight = collectionView.frame.height / 4 - 1
+
+        return CGSize(width: cellWidth, height: cellHeight)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 1.0
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 1.0
     }
 }
