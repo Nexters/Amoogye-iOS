249a2306f89940f0b644b51609cdf52d9bbd64ce
tngusmiso <k1miso012@gmail.com>
2019-09-03 14:44:43 +0900

    iOS-#83 [계산기] Show & Hide Portion Mode - srcPortionView, dstPortionView 알파값 조정 - 나머지 뷰들은 portionView가 나타나고 사라지는 것에 맞춰서 위치 이동

--- Amoogye/Calculator/CalculatorViewController + interaction.swift
+++ Amoogye/Calculator/CalculatorViewController + interaction.swift
@@ -16,6 +16,7 @@ extension CalculatorViewController {
             mode = .PortionOnly
             offMode(sender: meterialModeButton)
             onMode(sender: portionModeButton)
+            showPortionInputButton()
 
         case .PortionOnly:
             mode = .Both
@@ -35,16 +36,19 @@ extension CalculatorViewController {
             mode = .Both
             onMode(sender: portionModeButton)
             onMode(sender: plusLabel)
+            showPortionInputButton()
 
         case .PortionOnly:
             mode = .MeterialOnly
             onMode(sender: meterialModeButton)
             offMode(sender: portionModeButton)
+            hidePortionInputButton()
 
         case .Both:
             mode = .MeterialOnly
             offMode(sender: portionModeButton)
             offMode(sender: plusLabel)
+            hidePortionInputButton()
         }
     }
 
@@ -66,5 +70,33 @@ extension CalculatorViewController {
     func offMode(sender: UILabel) {
         sender.textColor = UIColor.amDarkBlueGreyWithOpacity(opacity: 0.2)
         sender.font = .systemFont(ofSize: 24)
+    }
+
+    func hidePortionInputButton() {
+        UIView.animate(withDuration: 0.2, animations: ({
+            self.srcPortionView.alpha  = 0
+            self.dstPortionView.alpha  = 0
+
+            self.srcQuantityView.transform = CGAffineTransform(translationX: -self.srcPortionView.frame.width, y: 0)
+            self.srcMeterialView.transform = CGAffineTransform(translationX: -self.srcPortionView.frame.width, y: 0)
+            self.srcFromView.transform = CGAffineTransform(translationX: -self.srcPortionView.frame.width, y: 0)
+
+            self.dstToolView.transform = CGAffineTransform(translationX: -self.dstPortionView.frame.width, y: 0)
+            self.dstToView.transform = CGAffineTransform(translationX: -self.dstPortionView.frame.width, y: 0)
+        }))
+    }
+
+    func showPortionInputButton() {
+        UIView.animate(withDuration: 0.2, animations: ({
+            self.srcPortionView.alpha  = 1
+            self.dstPortionView.alpha  = 1
+
+            self.srcQuantityView.transform = .identity
+            self.srcMeterialView.transform = .identity
+            self.srcFromView.transform = .identity
+
+            self.dstToolView.transform = .identity
+            self.dstToView.transform = .identity
+        }))
     }
 }
