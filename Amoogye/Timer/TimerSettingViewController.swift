06ec7471861c6d03869a23f872054dcec1c7affc
tngusmiso <k1miso012@gmail.com>
2020-07-06 16:18:59 +0900

    iOS-#96 [타이머 Custom Input Button] Custom TextField대신 CustomInputButton으로 변경 - 오토레이아웃 다시 지정해야 함 - Numeric Keyboard로 입력할 때 주황색 글씨도 나오도록 해야 함

--- Amoogye/Timer/TimerSettingViewController.swift
+++ Amoogye/Timer/TimerSettingViewController.swift
@@ -14,11 +14,15 @@ class TimerSettingViewController: UIViewController {
     var timerModel = TimerModel.shared
     let colorGreen = UIColor(displayP3Red: 109/255, green: 212/255, blue: 0, alpha: 1)
 
-    var textfiledManager: CustomTextfieldManager?
+    var inputButtonManager: CustomInputButtonManager?
+    var hourInputButton: CustomInputButton = CustomInputButton()
+    var minInputButton: CustomInputButton = CustomInputButton()
+    var secInputButton: CustomInputButton = CustomInputButton()
 
-    @IBOutlet weak var hourTextfield: CustomTextField!
-    @IBOutlet weak var minTextfield: CustomTextField!
-    @IBOutlet weak var secTextfield: CustomTextField!
+    @IBOutlet weak var hourInputView: UIView!
+    @IBOutlet weak var minInputView: UIView!
+    @IBOutlet weak var secInputView: UIView!
+
     @IBOutlet weak var secLabel: UILabel!
     @IBOutlet weak var alertLabel: UILabel!
 
@@ -29,7 +33,12 @@ class TimerSettingViewController: UIViewController {
         super.viewDidLoad()
 
         numericKeyboardView.delegate = self
-        textfiledManager = CustomTextfieldManager(hourTextfield, minTextfield, secTextfield)
+        hourInputView.addSubview(hourInputButton)
+        minInputView.addSubview(minInputButton)
+        secInputView.addSubview(secInputButton)
+
+        inputButtonManager = CustomInputButtonManager(hourInputButton, minInputButton, secInputButton)
+        inputButtonManager?.focusOn(button: hourInputButton)
         setupButtonStyle(startButton)
 
         // 최초 실행 시 이전에 저장된 상태 불러오기
@@ -67,7 +76,7 @@ class TimerSettingViewController: UIViewController {
     }
 
     @IBAction func clickStart(_ sender: Any) {
-        textfiledManager?.focusOutAll()
+        inputButtonManager?.focusOutAll()
 
         switch timerModel.getState() {
         case .Ready: // 시작 버튼 클릭
@@ -127,16 +136,13 @@ extension TimerSettingViewController {
         let min = Int(time) % 3600 / 60
         let sec = Int(time) % 3600 % 60
 
-        hourTextfield.text = String(hour)
-        minTextfield.text = String(min)
-        secTextfield.text = String(sec)
+        hourInputButton.setTitle(String(hour), for: .normal)
+        minInputButton.setTitle(String(min), for: .normal)
+        secInputButton.setTitle(String(sec), for: .normal)
     }
 
-    func setTimerTextfieldsEnable(booltype: Bool) {
-        hourTextfield.isEnabled = booltype
-        minTextfield.isEnabled = booltype
-        secTextfield.isEnabled = booltype
-        textfiledManager?.focusOutAll()
+    func setTimerInputButtonEnable(booltype: Bool) {
+//        inputButtonManager?.focusOutAll()
     }
 
     // MARK: - 상태에 따른 화면 전환
@@ -145,7 +151,7 @@ extension TimerSettingViewController {
         secLabel.text = "초 뒤에"
         alertLabel.text = "알려드릴게요!"
         startButton.setTitle("시작하기", for: .normal)
-        setTimerTextfieldsEnable(booltype: true)
+        setTimerInputButtonEnable(booltype: true)
         numericKeyboardView.isHidden = false
     }
 
@@ -154,7 +160,7 @@ extension TimerSettingViewController {
         secLabel.text = "초가"
         alertLabel.text = "끝났습니다!"
         startButton.setTitle("종료", for: .normal)
-        setTimerTextfieldsEnable(booltype: false)
+        setTimerInputButtonEnable(booltype: false)
         numericKeyboardView.isHidden = true
     }
 
@@ -166,14 +172,17 @@ extension TimerSettingViewController {
 
     // MARK: - 변환 관련 함수
     func getSecond() -> Int {
-        let hour: Int = textfieldToInt(tf: hourTextfield)
-        let min: Int = textfieldToInt(tf: minTextfield)
-        let sec: Int = textfieldToInt(tf: secTextfield)
+        let hour: Int = inputBtnTitleToInt(inputBtn: hourInputButton)
+        let min: Int = inputBtnTitleToInt(inputBtn: minInputButton)
+        let sec: Int = inputBtnTitleToInt(inputBtn: secInputButton)
+//        let hour: Int = textfieldToInt(tf: hourTextfield)
+//        let min: Int = textfieldToInt(tf: minTextfield)
+//        let sec: Int = textfieldToInt(tf: secTextfield)
         return hour * 3600 + min * 60 + sec
     }
 
-    func textfieldToInt(tf: UITextField) -> Int {
-        guard let text = tf.text else {
+    func inputBtnTitleToInt(inputBtn: UIButton) -> Int {
+        guard let text = inputBtn.title(for: .normal) else {
             return 0
         }
         return Int(text) ?? 0
@@ -182,55 +191,53 @@ extension TimerSettingViewController {
 
 extension TimerSettingViewController: NumericKeyboardDelegate {
     func inputNumber(number newValue: String) {
-        guard let textField = textfiledManager?.focusedTextField else {
+        guard let inputButton = inputButtonManager?.focusedButton else {
             return
         }
-        guard let text = textField.text else {
+        guard let text = inputButton.title(for: .normal) else {
             return
         }
 
         if text == "0" {
-            textField.text = newValue
+            inputButton.setTitle(newValue, for: .normal)
         } else {
-            textField.text = text + newValue
+            inputButton.setTitle(text+newValue, for: .normal)
         }
     }
 
     func deleteValue() {
-        guard let textField = textfiledManager?.focusedTextField else {
+        guard let inputButton = inputButtonManager?.focusedButton else {
             return
         }
-        guard let text = textField.text else {
+        guard let text = inputButton.title(for: .normal) else {
             return
         }
-
         if text.count > 1 {
             let end = text.index(before: text.endIndex)
-            textField.text = String(text[..<end])
+            inputButton.setTitle(String(text[..<end]), for: .normal)
         } else {
-            textField.text = "0"
+            inputButton.setTitle("0", for: .normal)
         }
     }
 
     func inputDot() {
-        guard let textField = textfiledManager?.focusedTextField else {
+        guard let inputButton = inputButtonManager?.focusedButton else {
             return
         }
-        guard let text = textField.text else {
+        guard let text = inputButton.title(for: .normal) else {
             return
         }
-
-        textField.text = text + "."
+        inputButton.setTitle(text+".", for: .normal)
     }
 
     func getLastInputValue() -> String {
-        guard let textField = textfiledManager?.focusedTextField else {
-            return ""
-        }
-        guard let text = textField.text else {
-            return ""
-        }
 
+        guard let inputButton = inputButtonManager?.focusedButton else {
+            return ""
+        }
+        guard let text = inputButton.title(for: .normal) else {
+            return ""
+        }
         let lastIndex = text.index(before: text.endIndex)
         return String(text[lastIndex])
     }
